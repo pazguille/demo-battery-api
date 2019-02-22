79928efb802cb938a5f8e6fb10a524d7b0efac21
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-02-22 11:23:04 +0200

    add Cypress tests

---
+++ cypress/integration/spec.js
@@ -0,0 +1,45 @@
+// enables intelligent code completion for Cypress commands
+// https://on.cypress.io/intelligent-code-completion
+/// <reference types="Cypress" />
+
+describe('battery test', () => {
+  it('shows battery status', function () {
+    cy.visit('/')
+    cy.get('.battery-percentage').should('be.visible')
+  })
+
+  context('navigator.battery', () => {
+    it('shows battery status of 50%', function () {
+      cy.visit('/', {
+        onBeforeLoad (win) {
+          win.navigator.battery = {
+            level: 0.5,
+            charging: false,
+            addEventListener: () => {}
+          }
+        }
+      })
+      cy.get('.battery-percentage').should('be.visible')
+    })
+  })
+
+  context('navigator.getBattery', () => {
+    it.only('shows battery status of 75%', function () {
+      cy.visit('/', {
+        onBeforeLoad (win) {
+          delete win.navigator.battery
+          win.navigator.getBattery = cy
+            .stub()
+            .resolves({
+              level: 0.75,
+              charging: false,
+              addEventListener: () => {}
+            })
+            .as('getBattery')
+        }
+      })
+      cy.contains('.battery-percentage', '75%')
+      cy.get('@getBattery').should('have.been.calledOnce')
+    })
+  })
+})
